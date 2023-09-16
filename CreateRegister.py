7a2643994925cfdd53690c9cd0630f052b93225f
Cristian G <cristian.guerrerobalber@gmail.com>
2023-09-16 23:23:49 +0200

    Se ha reemplazado en el código el registro en excel por el registro en una base de datos sqlite3

--- CreateRegister.py
+++ CreateRegister.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import datetime
 import openpyxl
+import sqlite3
 
 def main(lista_usuarios):
     print("Actualizando registro...")
@@ -14,12 +15,6 @@ def main(lista_usuarios):
     print(date_now)
     print(hour_now)
 
-    #Si ya existiese, se leen los datos del excel de output previo
-    ruta_excel = r"C:\Users\Cristian\Documents\5- Educación\Python\0-Aprendizaje\5-Automatizaciones web con SELENIUM\1-Chat Nicks\Z-Outputs\Output.xlsx"
-    #Se especifican la hoja y las columnas para que no haya discordancia entre lo leido y lo que se va a añadir
-    nombre_hoja = "Registro"
-    columnas = ['Nick', 'IP', 'Fecha', 'Hora']
-    previo_df = pd.read_excel(ruta_excel, sheet_name=nombre_hoja, usecols = columnas)
     #Se arreglan los datos: el segundo valor de las listas de lista, incluye en parte al nick, y se debe separar, ya que
     #lo que es "invariable" es el IP
     index = 0
@@ -33,32 +28,45 @@ def main(lista_usuarios):
                 break
         solo_IP_texto = IP_compuesto_texto[pos_inicial + 1:pos_final]
         sublista[1] = solo_IP_texto
-    #Se preparan los datos para el dataframe
-    datos = lista_usuarios
+
     #Se agrega la fecha y la hora a los datos
     for sublista in lista_usuarios:
         sublista.append(date_now)
         sublista.append(hour_now)
 
-    #Se crea el dataframe
-    nuevo_df = pd.DataFrame(datos, columns = columnas)
-    #Se agrega el dataframe creado al previo
-    final_df = pd.concat([previo_df, nuevo_df], ignore_index=True)
-    #print(final_df)
+    # Se accede / crea la base de datos (solo se crea si no existiese ya). La siguiente línea, tan solo almacena la ruta
+    ruta_bdd = r"C:\Users\Cristian\Documents\5- Educación\Python\0-Aprendizaje\5-Automatizaciones web con SELENIUM\1-Chat Nicks\base_de_datos\usuarios_chat.db"
 
-    # Leer el archivo Excel existente y cargarlo en un diccionario de DataFrames
-    with pd.ExcelFile(ruta_excel) as xlsx:
-        hojas_dict = {sheet_name: xlsx.parse(sheet_name) for sheet_name in xlsx.sheet_names}
+    # Se crea un query para crear la tabla registro solo en caso de que no exista
+    with sqlite3.connect(ruta_bdd) as conn:
+        cursor = conn.cursor()
+        # Se especifican la hoja y las columnas para que no haya discordancia entre lo leido y lo que se va a añadir
+        nombre_tabla_registro = "Registro"
+        columnas_registro = ['Nick', 'IP', 'Fecha', 'Hora']
+        cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS "{nombre_tabla_registro}" (
+        "id_registro" INTEGER,
+        "{columnas_registro[0]}" TEXT,
+        "{columnas_registro[1]}" TEXT,
+        "{columnas_registro[2]}" TEXT,
+        "{columnas_registro[3]}" TEXT,
+        PRIMARY KEY("id_registro" AUTOINCREMENT)
+        )
+        ''')
 
-    hojas_dict[nombre_hoja] = final_df
-
-    # Guardar el diccionario de DataFrames en el mismo archivo Excel
-    with pd.ExcelWriter(ruta_excel, engine='openpyxl') as writer:
-        for sheet_name, df in hojas_dict.items():
-            df.to_excel(writer, sheet_name=sheet_name, index=False)
+        #Ahora se introducen los datos en la tabla
+        for sublista in lista_usuarios:
+            cursor.execute(f'''
+                    INSERT INTO {nombre_tabla_registro}
+                    ({columnas_registro[0]},{columnas_registro[1]},{columnas_registro[2]},{columnas_registro[3]})
+                    VALUES ("{sublista[0]}", "{sublista[1]}", "{sublista[2]}", "{sublista[3]}")
+                    ''')
+        #Se envían y guardan los cambios
+        conn.commit()
 
     print("Registro actualizado")
 
+#Este código sirve para realizar pruebas ejecutando el código desde este archivo py
 if __name__ == "__main__":
     lista_usuarios = [
         ['Juan', 'juan@iwehjakjdf.IP'],
--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import CreateRegister
 
 lista_usuarios = GetUsersList.main()
 
-print("hola git")
-
 print(f'Cantidad de usuarios: {len(lista_usuarios)}')
 
 print(lista_usuarios)
